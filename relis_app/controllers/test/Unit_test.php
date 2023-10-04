f93ea28df367f98ac7e9d1cba97e625e8dc7f759
christmalak90 <christ.malak@gmail.com>
2023-10-04 13:48:56 -0400

    commit134

--- relis_app/controllers/test/Unit_test.php
+++ relis_app/controllers/test/Unit_test.php
@@ -31,7 +31,7 @@ class Unit_test extends CI_Controller
     function __construct()
     {
         parent::__construct();
-        $this->load->helper('test/curl');
+        $this->load->helper('test/christ');
         $this->load->helper('test/UserUnitTest');
         // $this->load->helper('test/project_test');
         // $this->load->helper('test/paper_test');
